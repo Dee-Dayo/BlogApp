95a5bc2f548887fcac97e1f437d38feb6d6d8152
Dee-dayo <darhyor2050@gmail.com>
2024-03-27 14:30:24 +0100

    added test

--- src/test/java/africa/semicolon/data/repository/PostRepositoryTest.java
+++ src/test/java/africa/semicolon/data/repository/PostRepositoryTest.java
@@ -3,10 +3,10 @@ package africa.semicolon.data.repository;
 import africa.semicolon.data.model.Post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,8 +21,11 @@ class PostRepositoryTest {
         post.setId("1");
         post.setTitle("title");
         post.setContent("content");
-
         postRepository.save(post);
+        var foundPost = postRepository.findById("1");
+
+        assertThat(postRepository.count(), is(1L));
+        assertThat(foundPost, is(notNullValue()));
 
         assertEquals(1, postRepository.count());
     }
